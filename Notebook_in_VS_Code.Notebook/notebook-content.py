9544df941669c3873dfd51d1a59e2f5a818da436
RamReddy Myla(Ram) <dwramreddy@gmail.com>
2026-05-30 06:18:52 -0700

    Committing 1 item from workspace 12f495ce-6aee-4531-98b5-aad2886e82a5

--- Notebook_in_VS_Code.Notebook/notebook-content.py
+++ Notebook_in_VS_Code.Notebook/notebook-content.py
@@ -6,7 +6,21 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "c8964cc6-2403-4b70-83c5-e4b65f7da4c3",
+# META       "default_lakehouse_name": "bronze",
+# META       "default_lakehouse_workspace_id": "12f495ce-6aee-4531-98b5-aad2886e82a5",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "c8964cc6-2403-4b70-83c5-e4b65f7da4c3"
+# META         },
+# META         {
+# META           "id": "35333e4a-85d9-4ce1-9d53-0c832c036b85"
+# META         }
+# META       ]
+# META     }
+# META   }
 # META }
 
 # CELL ********************
